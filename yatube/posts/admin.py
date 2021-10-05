16b2fd70e50c386f8f17999c1b1a20bf2a59c29d
deadplut <suganoff@yandex.ru>
2021-10-05 17:33:21 +0300

    0.1.0

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import *
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -13,5 +13,16 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"  # это свойство сработает для всех колонок: где пусто - там будет эта строка
 
 
+class GroupAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("pk", "title", "slug", "description")
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("title",)
+    # добавляем возможность фильтрации по дате
+    list_filter = ("title",)
+    empty_value_display = "-пусто-"  # это свойство сработает для всех колонок: где пусто - там будет эта строка
+
+
 # при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -8,15 +8,25 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    group = models.ForeignKey('Group', on_delete=models.PROTECT, blank=True, null=True, verbose_name='Категория')
+
+    class Meta:
+        ordering =['-pub_date']
 
 
-'''class Event(models.Model):
-    name = models.TextField(max_length=200)
-    start_at = models.DateTimeField("date start", auto_now_add=True)
+class Group(models.Model):
+    title = models.CharField(max_length=100, db_index=True, verbose_name='Категории')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     description = models.TextField()
-    contact = models.EmailField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="events")
-    location = models.TextField(max_length=400)
-    pass
-'''
+
+    def __str__(self):
+        return self.title
+
+    '''def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']'''
 
